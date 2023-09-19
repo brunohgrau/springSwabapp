97e05a47a3651b2dddf4e988288115c310e16d6e
Bruno <brunohgrau99@gmail.com>
2023-09-19 04:58:48 -0300

    Initial commit

---
+++ src/main/java/bruno/springframework/springSwabapp/SpringSwabappApplication.java
@@ -0,0 +1,13 @@
+package bruno.springframework.springSwabapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwabappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwabappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bruno/springframework/springSwabapp/SpringSwabappApplicationTests.java
@@ -0,0 +1,13 @@
+package bruno.springframework.springSwabapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwabappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
