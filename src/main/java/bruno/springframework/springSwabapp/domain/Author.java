86ce80957cdb5313d20a2e13131cbd05614abb21
Bruno <brunohgrau99@gmail.com>
2023-09-19 05:27:01 -0300

    First Commit

---
+++ src/main/java/bruno/springframework/springSwabapp/domain/Author.java
@@ -0,0 +1,9 @@
+package bruno.springframework.springSwabapp.domain;
+import java.util.Set;
+public class
+Author {
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+}
---
+++ src/main/java/bruno/springframework/springSwabapp/domain/Book.java
@@ -0,0 +1,9 @@
+package bruno.springframework.springSwabapp.domain;
+import java.util.Set;
+
+public class Book {
+
+    private String title;
+    private String isbn;
+
+}
